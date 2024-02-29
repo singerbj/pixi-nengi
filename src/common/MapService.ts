13ca1c895ed3a65abc46b167330f69e31e3d90bd
Benjamin Singer <benjaminjsinger@gmail.com>
2024-02-29 16:21:41 -0500

    add more to the map

--- src/common/MapService.ts
+++ src/common/MapService.ts
@@ -11,12 +11,33 @@ const leftWall = new MapObject(-1000, -500, MAP_OBJECT_STANDARD_FACTOR, 1000);
 const rightWall = new MapObject(1000, -500, MAP_OBJECT_STANDARD_FACTOR, 1000);
 const ceiling = new MapObject(-1000, -500, 2000, MAP_OBJECT_STANDARD_FACTOR);
 const middleObstacle = new MapObject(
-  -MAP_OBJECT_STANDARD_FACTOR / 2,
+  (-MAP_OBJECT_STANDARD_FACTOR * 6) / 2,
   400,
-  MAP_OBJECT_STANDARD_FACTOR,
+  MAP_OBJECT_STANDARD_FACTOR * 6,
   100
 );
 
+const leftWallJumpWall = new MapObject(
+  -850,
+  -300,
+  MAP_OBJECT_STANDARD_FACTOR,
+  700
+);
+const rightWallJumpWall = new MapObject(
+  850,
+  -300,
+  MAP_OBJECT_STANDARD_FACTOR,
+  700
+);
+
 export const getMap = (): MapObject[] => {
-  return [floor, leftWall, rightWall, ceiling, middleObstacle];
+  return [
+    floor,
+    leftWall,
+    rightWall,
+    ceiling,
+    middleObstacle,
+    leftWallJumpWall,
+    rightWallJumpWall,
+  ];
 };
